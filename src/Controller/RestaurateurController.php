eda2e74431832a3a2c3d0b2d1b35f35741c98b88
Maxence <maxence.crosse@ynov.com>
2020-12-16 17:26:49 +0100

    restaurateur crud plat avancé

--- src/Controller/RestaurateurController.php
+++ src/Controller/RestaurateurController.php
@@ -6,6 +6,7 @@ use App\Entity\Plat;
 use App\Entity\Restaurant;
 use App\Form\PlatType;
 use App\Form\RestaurantType;
+use App\Repository\PlatRepository;
 use App\Repository\RestaurantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,10 +20,14 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class RestaurateurController extends AbstractController
 {
+    #Pourquoi pas une page home "/" pour afficher ses stats 
+
+    ##################### Restaurant #############################
+
     /**
-     * @Route("/myrestaurants", name="restaurateur.index")
+     * @Route("/myrestaurants", name="restaurateur.restaurant.all", methods={"GET"})
      */
-    public function index(RestaurantRepository $repo): Response
+    public function allRestaurants(RestaurantRepository $repo): Response
     {
         $restaurants = $repo->getByIdUser($this->getUser());
         return $this->render('restaurateur/restaurant/index.html.twig', [
@@ -31,10 +36,23 @@ class RestaurateurController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="restaurateur.new", methods={"GET","POST"})
+     * @Route("/myrestaurants/{id}", name="restaurateur.restaurant.one", methods={"GET"})
      */
-    public function newRestaurant(Request $request, EntityManagerInterface $em): Response
+    public function oneRestaurant(Restaurant $restaurant)//rajouter un if ou genre on verif si ce resto appartient bien au user
     {
+        return $this->render('restaurateur/restaurant/restaurant.html.twig',[
+            'restaurant' => $restaurant
+        ]);
+    }
+
+    #-----------------CRUD--------------#
+
+    /**
+     * @Route("myrestaurants/new", name="restaurateur.restaurant.new", methods={"GET","POST"})
+     */
+    public function newRestaurant(Request $request, EntityManagerInterface $em, Restaurant $Restaurant): Response
+    {
+        $idRestaurant = $Restaurant->getId();
         $restaurant = new Restaurant();
         $form = $this->createForm(RestaurantType::class, $restaurant);
         $form->handleRequest($request);
@@ -55,30 +73,96 @@ class RestaurateurController extends AbstractController
             $em->persist($restaurant);
             $em->flush();
         
-            return $this->redirectToRoute('restaurateur.index');
+            return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);
         }
 
         return $this->render('restaurateur/restaurant/new.html.twig', [
             'restaurant' => $restaurant,
             'form' => $form->createView(),
+            'idRestaurant' => $idRestaurant
         ]);
     }
 
     /**
-     * @Route("/myrestaurants/{id}", name="restaurateur.restaurant")
+     * @Route("myrestaurants/{id}/edit", name="restaurateur.restaurant.edit", methods={"GET","POST"})
      */
-    public function oneRestaurant(Restaurant $restaurant)//rajouter un if ou genre on verif si ce resto appartient bien au user
+    public function editRestaurant(Request $request, Restaurant $restaurant, EntityManagerInterface $em): Response
     {
-        return $this->render('restaurateur/restaurant/restaurant.html.twig',[
-            'restaurant' => $restaurant
+        $idRestaurant = $restaurant->getId();
+        $form = $this->createForm(RestaurantType::class, $restaurant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile */
+            $file = $form->get('logoFile')->getData();
+
+            $filename = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move(
+                $this->getParameter('upload_directory'),
+                $filename
+            );
+
+            $restaurant->setLogo($filename);
+            $em->persist($restaurant);
+            $em->flush();
+
+            return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);
+        }
+
+        return $this->render('restaurateur/restaurant/edit.html.twig', [
+            'restaurant' => $restaurant,
+            'form' => $form->createView(),
+            'idRestaurant' => $idRestaurant
         ]);
     }
 
     /**
-     * @Route("/myrestaurants/{id}/newplat", name="restaurateur.newplat")
+     * @Route("myrestaurants/{id}", name="restaurateur.restaurant.delete", methods={"DELETE"})
+     */
+    public function deleteRestaurant(Request $request, Restaurant $restaurant): Response
+    {
+        $idRestaurant = $restaurant->getId();
+        if ($this->isCsrfTokenValid('delete'.$restaurant->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($restaurant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);
+    }
+
+    ###############################PLAT#################################################
+
+    /**
+     * @Route("/myrestaurants/{id}/plats", name="restaurateur.plat.all", methods={"GET"})
+     */
+    public function allPlats(Restaurant $restaurant, PlatRepository $repo)
+    {
+        $plats = $repo->getByID($restaurant->getId());
+        return $this->render('restaurateur/plat/index.html.twig',[
+            'plats' => $plats,
+        ]);
+    }
+
+    /**
+     * @Route("/myrestaurants/{restaurant}/plats/{plat}", name="restaurateur.plat.one", methods={"GET"}, requirements={"plat"="\d+"})
+     */
+    public function onePlat(Restaurant $restaurant,Plat $plat)
+    {
+        return $this->render('restaurateur/plat/plat.html.twig',[
+            'restaurant' => $restaurant,
+            'plat' => $plat
+        ]);
+    }
+
+    #----------------------CRUD--------------------------#
+
+    /**
+     * @Route("/myrestaurants/{restaurant}/plats/newplat", name="restaurateur.plat.new", methods={"GET","POST"})
      */
     public function newPlat(Request $request, EntityManagerInterface $em, Restaurant $restaurant)
     {
+        $idRestaurant = $restaurant->getId();
         $plat = new Plat();
         $form = $this->createForm(PlatType::class, $plat);
         $form->handleRequest($request);
@@ -99,12 +183,61 @@ class RestaurateurController extends AbstractController
             $em->persist($plat);
             $em->flush();
             
-            return $this->redirectToRoute('restaurateur.index');//changer et mettre la liste des plats
+            return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);//changer et mettre la liste des plats
         }
 
         return $this->render('restaurateur/plat/new.html.twig', [
             'plat' => $plat,
             'form' => $form->createView(),
+            'idRestaurant' => $idRestaurant
         ]);
+    }
+
+    /**
+     * @Route("/myrestaurants/{restaurant}/plats/{plat}/edit", name="restaurateur.plat.edit", methods={"GET","POST"})
+     */
+    public function editPlat(Request $request, Restaurant $restaurant, Plat $plat,EntityManagerInterface $em): Response
+    {
+        $idRestaurant = $restaurant->getId();
+        $form = $this->createForm(PlatType::class, $plat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile */
+            $file = $form->get('imageFile')->getData();
+
+            $filename = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move(
+                $this->getParameter('upload_directory'),
+                $filename
+            );
+
+            $plat->setImage($filename);
+            $em->persist($plat);
+            $em->flush();
+
+            return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);
+        }
+
+        return $this->render('restaurateur/plat/edit.html.twig', [
+            'plat' => $plat,
+            'form' => $form->createView(),
+            'idRestaurant' => $idRestaurant
+        ]);
+    }
+
+    /**
+     * @Route("myrestaurants/{restaurant}/plats/{plat}", name="restaurateur.plat.delete", methods={"DELETE"})
+     */
+    public function deletePlat(Request $request,Restaurant $restaurant, Plat $plat): Response
+    {
+        $idRestaurant = $restaurant->getId();
+        if ($this->isCsrfTokenValid('delete'.$plat->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($plat);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('restaurateur.plat.all',['id' => $idRestaurant]);
     }
 }
